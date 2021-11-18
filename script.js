f326a1c5631b433f957ea01a77f3b652bdf638a4
Rafael <fael.vargas@hotmail.com>
2021-11-18 14:06:44 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function dayTime(){
+    var date = new Date();
+    var h =  18 //date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    console.log(h + ' horas, ' + m + ' minutos e ' + s + ' segundos.')
+
+    if (h >= '0' && h <= '12') {
+        document.getElementById('color').classList.remove('evening');
+        document.getElementById('color').classList.remove('night');
+        document.getElementById('color').classList.add('morning');
+        document.querySelector('.hour').innerHTML = ('Agora é ' + h + ' horas, ' + m + ' minutos e ' + s + ' segundos.<br>Bom dia!')
+        console.log("Bom dia!");
+    }
+
+    if (h >= '13' && h < '18') {
+        document.getElementById('color').classList.remove('morning');
+        document.getElementById('color').classList.remove('night');
+        document.getElementById('color').classList.add('evening');
+        document.querySelector('.hour').innerHTML = ('Agora é ' + h + ' horas, ' + m + ' minutos e ' + s + ' segundos.<br>Boa tarde!')
+        console.log("Boa tarde!");
+    }
+
+    if (h >= '18' && h <= '23') {
+        document.getElementById('color').classList.remove('morning');
+        document.getElementById('color').classList.remove('evening');
+        document.getElementById('color').classList.add('night');
+        document.querySelector('.hour').innerHTML = ('Agora é ' + h + ' horas, ' + m + ' minutos e ' + s + ' segundos.<br>Boa noite!')
+        console.log("Boa noite!");
+    }
+
+    setTimeout(dayTime, 1000);
+}
+
+dayTime();
